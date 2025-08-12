fdfab53de0df64f4d0bbc4ba1bbdf3c8aa26c12d
Anton <a.stryzhavchuk@gmail.com>
2025-08-12 13:50:37 -0400

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 
-export function getImagesByQuery(query, page) {
-  return axios
-    .get('https://pixabay.com/api/', {
-      params: {
-        key: '51711759-c98880f89c2a2cc3e02319f38',
-        q: query.trim(),
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 15, // кількість карток за запит
-        page: page,   // номер сторінки
-      },
-    })
-    .then(response => {
-      return response.data; // повертаємо весь об'єкт, щоб отримати totalHits
-    });
+export async function getImagesByQuery(query, page) {
+  const response = await axios.get('https://pixabay.com/api/', {
+    params: {
+      key: '51711759-c98880f89c2a2cc3e02319f38',
+      q: query.trim(),
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page: page,
+    },
+  });
+  return response.data; // повертаємо весь об'єкт data, щоб мати totalHits
 }
